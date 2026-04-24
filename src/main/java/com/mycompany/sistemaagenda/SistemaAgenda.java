018eecb0db6f40d8f8432f3205a10bb976caee04
Borghi0 <joaopborghi@gmail.com>
2026-04-24 13:10:09 -0300

    main

--- src/main/java/com/mycompany/sistemaagenda/SistemaAgenda.java
+++ src/main/java/com/mycompany/sistemaagenda/SistemaAgenda.java
@@ -1,9 +1,32 @@
 package com.mycompany.sistemaagenda;
 
+import com.github.weisj.darklaf.DarkLaf;
+import com.github.weisj.darklaf.LafManager;
+import com.mycompany.sistemaagenda.controller.DatabaseController;
+import com.mycompany.sistemaagenda.view.DbConnection;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 
 public class SistemaAgenda {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        LafManager.install(new com.github.weisj.darklaf.theme.OneDarkTheme());
+        try {
+            UIManager.setLookAndFeel(new DarkLaf());
+        }
+        catch (UnsupportedLookAndFeelException e) {}
+        
+        
+        DatabaseController dbCtrl = new DatabaseController();               
+        
+        if(dbCtrl.connect()){
+            //janela login            
+        }
+        else{            
+            DbConnection dbConWindow = new DbConnection();
+            dbConWindow.setVisible(true);
+        }
     }
 }
