06363fdba875b85fda25c80e68e4c01a3b32ca94
Borghi0 <joaopborghi@gmail.com>
2026-05-09 01:08:45 -0300

    janela de cadastro

---
+++ src/main/java/com/mycompany/sistemaagenda/controller/SignUpController.java
@@ -0,0 +1,44 @@
+package com.mycompany.sistemaagenda.controller;
+
+import com.mycompany.sistemaagenda.model.User;
+import com.mycompany.sistemaagenda.navigation.Navigator;
+import com.mycompany.sistemaagenda.service.LoginService;
+import com.mycompany.sistemaagenda.view.SignUp;
+
+
+public class SignUpController {
+    private final SignUp signUpWindow;
+    private final Navigator nav;
+    
+    public SignUpController(SignUp signUpWindow, Navigator nav){
+        this.signUpWindow = signUpWindow;
+        this.nav = nav;
+    }
+    
+    public void SignUp(String email, String name, String password, String cpassword){
+        if(email.isBlank() || name.isBlank() || password.isBlank()){
+            signUpWindow.showErrorMsg("Nenhum campo pode ser vazio", "Erro");
+            return;
+        }        
+        if(!password.equals(cpassword)){
+            signUpWindow.showErrorMsg("Senhas diferentes", "Erro");
+            return;
+        }       
+        
+        User user = new User(email, name, password, false);
+        
+        LoginService loginService = new LoginService();
+        try {
+            loginService.signUp(user);
+            signUpWindow.showMsg("Usuário cadastrado com sucesso!");
+            signUpWindow.clear();
+            nav.closeSignUp();            
+        } catch (Exception ex) {
+            signUpWindow.showErrorMsg(ex.getMessage(), "Erro");
+        }
+    }
+    
+    public void goBack(){
+        nav.closeSignUp();
+    }
+}
