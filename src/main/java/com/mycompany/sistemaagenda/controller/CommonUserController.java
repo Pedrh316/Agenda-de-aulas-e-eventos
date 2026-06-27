c27c5d49f4ae9d5fb041db34951226123f351823
ArthurHCaron <arthurhcaron@gmail.com>
2026-06-27 14:56:18 -0300

    Geraldo pdf de cancelamento

--- src/main/java/com/mycompany/sistemaagenda/controller/CommonUserController.java
+++ src/main/java/com/mycompany/sistemaagenda/controller/CommonUserController.java
@@ -11,6 +11,7 @@ import com.mycompany.sistemaagenda.service.PixService;
 import com.mycompany.sistemaagenda.service.UserService;
 import com.mycompany.sistemaagenda.view.CommonUser;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -55,6 +56,7 @@ public class CommonUserController {
                     
                 } catch(EmptyListException ex){
                     userWindow.showInfoMsg("Não há eventos disponíveis no momento.", "Que pena!");
+                    userWindow.clearTable(userWindow.getAvailableEventsTable());
                 } catch(ClassNotFoundException | SQLException ex){
                     userWindow.showErrorMsg(ex.getMessage(), "Erro ao atualizar tabela");
                 }
@@ -68,6 +70,7 @@ public class CommonUserController {
                     userWindow.updateTable(userWindow.getUserEventsTable(), events);
                 } catch(EmptyListException ex){
                     userWindow.showInfoMsg("Você ainda não se inscreveu em nenhum evento.", "Tabela");
+                    userWindow.clearTable(userWindow.getUserEventsTable());
                 } catch(ClassNotFoundException | SQLException ex){
                     userWindow.showErrorMsg(ex.getMessage(), "Erro ao atualizar tabela");
                 }
@@ -84,14 +87,14 @@ public class CommonUserController {
         BufferedImage qrcode;
         
         if(!userWindow.showQuestionMsg("Deseja se inscrever no evento?", "Inscrição")) return;
-        
+
         try{
             us.subscribeToEvent(Session.getLoggedUser(), room, dateTime);
-            
+
             if(fee > 0){
                 pix = PixService.generatePixPayload("equiperocketpagamentos@gmail.com", fee);
                 qrcode = PixService.generateQRCode(pix, 500, 500);
-                
+
                 userWindow.showPayMsg(qrcode);
             }
         } catch(InsertionFailedException ex){
@@ -100,6 +103,28 @@ public class CommonUserController {
             userWindow.showErrorMsg(ex.getMessage(), "Erro de conexão");
         } catch(WriterException we){
             userWindow.showErrorMsg(we.getMessage(), "Erro ao gerar QR Code");
+        }
+    }
+    
+    public void tableItemSelected(int room, LocalDate date, LocalTime time, float fee, String email){
+        boolean opt = false;
+        UserService us = new UserService();
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        
+        if(!userWindow.showQuestionMsg("Deseja cancelar inscrição no evento?", 
+                "Cancelamento de inscrição")) return;
+        
+        try{
+            us.unsubscribeToEvent(email, room, dateTime);
+            
+            opt = userWindow.showQuestionMsg("Deseja baixar comprovante de cancelamento?", "Cancelamento");
+            
+            if(opt == true) PixService.generateCancelTicket(email, date, time, fee);
+            
+        } catch(ClassNotFoundException | SQLException clnf_sql_e){
+            userWindow.showErrorMsg(clnf_sql_e.getMessage(), "Erro de conexão");
+        } catch(IOException ioe){
+            userWindow.showErrorMsg(ioe.getMessage(), "Erro de arquivo");
         }
     }
 }
